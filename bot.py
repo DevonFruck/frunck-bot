d12830f6c557a2ead9e980bcf9d96eba47a38a6f
DevonFruck <devonfruck@cmail.carleton.ca>
2021-08-05 11:26:39 -0400

    added test message

--- bot.py
+++ bot.py
@@ -6,16 +6,18 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-	print('Logged in as {0.user}.format(client)')
+    print('Logged in as {0.user}.format(client)')
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
+    if message.author == client.user:
+        return
 
-	if (message.content.startswith('!')):
-		await message.channel.send('Hello!')
+        if (message.content.startswith('!')):
+            if (message.content.contains('hi')):
+                await message.channel.send("Don't say hi to me!")
+            else:
+                await message.channel.send('Hello!')
 
-#client.run('ODcxNzYwOTUwMzAxMTc5OTM0.YQgApw.L0T8IzpiYeea2rXpIiK2aC7lKp8')
 load_dotenv()
 client.run(os.getenv('TOKEN'))
